a218a629a9b8edec1ec0feaa5ed84013eb678618
Gustavo lucena silva <gustavolucena12@gmail.com>
2021-11-30 17:14:28 -0300

    Adicionando funcionalidades

--- js/adcionarList.js
+++ js/adcionarList.js
@@ -1,20 +1,56 @@
 (function(){
 
-   const btnNovoItem = document.querySelector('.checklist__botao')
-   btnNovoItem.addEventListener('click', ()=> {
-
-      let conteudoInput = document.querySelector('.checklist__input').value
+   const adicionarItem = () =>{
+      const conteudoInput = document.querySelector('.checklist__input')
       const lista = document.querySelector('.checklist__lista')
 
-      const tarefa = document.createElement('li')
+      let tarefa = document.createElement('li')
       tarefa.classList.add('lista__item')
 
-      const paragrafo = document.createElement('p')
+      let paragrafo = document.createElement('p')
       paragrafo.classList.add('lista__paragrafo')
-
-      paragrafo.innerHTML = conteudoInput
+      paragrafo.innerHTML = conteudoInput.value
 
       tarefa.appendChild(paragrafo)
+      tarefa.appendChild(adicionarBtnConcluir())
+      tarefa.appendChild(adicionarBtnExcluir())
       lista.appendChild(tarefa)
-   }) 
+
+      conteudoInput.value = ' '
+   }
+
+   const adicionarBtnConcluir = ()=>{
+      let concluir = document.createElement('button')
+      concluir.classList.add('item__botao--completar')
+      concluir.textContent = 'Concluir'
+
+      concluir.addEventListener('click', concluirBtn)
+
+      return concluir
+   }
+
+   function concluirBtn(event){
+      let paiElement = event.target.parentElement
+      let paragrafo = paiElement.querySelector('p')
+      paragrafo.classList.toggle('botao--completar--done')
+   }
+
+   const adicionarBtnExcluir = ()=> {
+      let excluir = document.createElement('button')
+      excluir.classList.add('item__botao--excuir')
+      excluir.textContent = 'Excluir'
+
+      excluir.addEventListener('click', excluirBtn)
+
+      return excluir
+   }
+
+   function excluirBtn(event){
+      let paiElement = event.target.parentElement
+      paiElement.remove()
+   }
+
+   const btnNovoItem = document.querySelector('.checklist__botao')
+   btnNovoItem.addEventListener('click', adicionarItem)
+
 })()
